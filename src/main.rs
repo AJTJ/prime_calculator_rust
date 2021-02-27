07f3f790d224141d177ef85dc33c57ac120f397c
AJTJ <janke.aaron@gmail.com>
2021-02-27 15:35:50 -0400

    user can now input number

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+use std::io;
 fn main() {
+    let user_value: i32 = loop {
+        println!("Give a maximum number to find prime numbers within.");
+        let mut response = String::new();
+        io::stdin()
+            .read_line(&mut response)
+            .expect("Response Error");
+
+        println!("the response {}", response);
+
+        match response.trim().parse::<i32>() {
+            Ok(val) => {
+                break val;
+            }
+            Err(_) => println!("That is not a valid number, try again."),
+        };
+    };
+
+    print!("num is {}", user_value);
+
     let mut stored_primes = vec![];
     fn check_if_prime(x: i32, primes: &mut Vec<i32>) -> bool {
         if x % 2 == 0 {
@@ -15,7 +35,7 @@ fn main() {
         true
     }
 
-    let nums = 1..100;
+    let nums = 1..user_value;
     nums.for_each(|x| {
         println!(
             "is {} a prime number? {}",
